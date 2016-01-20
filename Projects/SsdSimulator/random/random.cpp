5fdb45bb0cbee7360195293ee3353a21b67e421b
shuke.wang <shuke.wang@memblaze.com>
2016-01-20 18:55:46 +0800

    add random project

---
+++ Projects/SsdSimulator/random/random.cpp
@@ -0,0 +1,29 @@
+// random.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    srand(time(NULL));
+    
+    for (int i = 0; i < 50; i++)
+    {
+        printf("%d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n", rand() % 10
+            , rand() % 10
+            , rand() % 10
+            , rand() % 10
+            , rand() % 10
+            , rand() % 10
+            , rand() % 10
+            , rand() % 10
+            , rand() % 10
+            , rand() % 10);
+    }
+
+    return 0;
+}
+
